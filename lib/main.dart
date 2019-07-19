d77847c69b81029383584d8c9cc2d3aa5b9af71b
GorCat <745318341@qq.com>
2019-07-19 17:36:21 +0800

    3.map

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,81 @@
 // import 'package:flutter/material.dart';
 
 void main() {
-  sets();
+  maps();
+}
+
+// === 3.maps ===
+void maps() {
+  // map: key-value 无序集合
+
+  // 1.init
+  var gifts = {
+    // key: value
+    'first': 'partridge',
+    'second': 'turtledoves',
+    'fifth': 'golen rings'
+  }; // Map<String: String>
+
+  var nobleGases = {
+    2: 'helium',
+    10: 'neon',
+    18: 'argon',
+  }; // Map<Int: String>
+
+  // 2.add a key-value pair
+  gifts['first'] = 'partridge';
+  nobleGases[2] = 'neon';
+
+  // 3.if you look for a key that isn't in a map, you get a null in return.
+  var gifts1 = {'first': 'partiridge'};
+  assert(gifts1['fifth'] == null);
+
+  // 4.use length to get the number of key-value pairs in the map:
+  print('4.length = ${gifts1.length}');
+
+  // 5.const map
+  final constantMap = const {
+    2: 'helium',
+    10: 'neon'
+  };
+  // constantMap[2] = 'Helium'l // Uncommenting this causes an error
+
+  // 6.spread operator (...) & null-aware spread operator (...?)
+  var elements = {0: 'spread operator', ...constantMap};
+  print('6.$elements');
+
+  var nullMap;
+  elements = {0: 'null-aware spread operator', ...?nullMap};
+  print('6.$elements');
+
+  // 7. collection if
+  elements = {
+    0: 'collection if',
+    if (constantMap[2] == 'helium') 1: 'helium',
+  };
+  print('7.$elements');
+
+  // 8. collection for
+  var array = [1, 2, 3, 4];
+  elements = {
+    0: 'colleciton for',
+    for (var i in array) i : '$i',
+  };
+  print('8.$elements');
+
 }
 
 // === 2.sets ===
 void sets() {
-  // set：无序且唯一值的集合，和 iOS 中的 set 差不多
+  // set：无序且唯一值的集合
 
   // 1.init
   var halogens = {'fluorine', 'chlorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
   var names = <String>{};
+  var numbers = {1, 2, 3};
   // Set<String> halogens = {}; // dThis works, too.
 
-  // {} defaults to the Map type.
+  // {} defaults to the Map type, Map<dynamic: dynamic>.
   // var halogens = {} // Creates a map, not a set.
 
   // 只会打印 1 个 chlorine
@@ -34,6 +96,31 @@ void sets() {
   };
   // constantSet.add('helium'); 
   print("3.$constantSet");
+
+  // 4.spread operator (...) and null-aware spread operator (...?)
+  elements = {'spread operator', ...constantSet};
+  print('4.$elements');
+
+  var nullsets;
+  names = {'null-aware spread operator', ...?nullsets};
+  print('4.$names');
+
+  // 5.collection if
+  numbers = {
+    1,
+    2,
+    if (numbers.contains(3)) 4,
+    if (numbers.contains(4)) 5,
+  };
+  print('5.$numbers');
+
+  // 6.collection for
+  var array = [1, 2, 3, 4];
+  numbers = {
+    for (var i in array) i,
+    10,
+  };
+  print('6.$numbers');
 }
 
 // === 1.colleciton ===
