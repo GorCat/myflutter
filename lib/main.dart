8a932f0f404013b3f8539884bb672191a17e21c2
GorCat <luoyangchengguo@gmail.com>
2019-07-21 23:06:51 +0800

    1.the hello word of flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,40 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
 import 'basedart.dart';
+import 'baseflutter.dart';
 
 void main() {
-  maps();
+ runApp(MyApp());
+}
+
+// 2.package
+// class RandomWords extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() {
+//     // TODO: implement createState
+//     new RandomWordsState();
+//   }
+// }
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+// 1.hello world
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'app title',
+      home:  Scaffold(
+        appBar: AppBar(
+          title:  Text('bar title'),
+        ),
+        body:  Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
 }
