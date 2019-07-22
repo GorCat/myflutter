ab14ce1eb17229f8c3cce866584dc293a5ba0b56
GorCat <745318341@qq.com>
2019-07-22 11:50:39 +0800

    use package

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-import 'basedart.dart';
-import 'baseflutter.dart';
+// import 'basedart.dart';
+// import 'baseflutter.dart';
 
 void main() {
  runApp(MyApp());
 }
 
 // 2.package
-// class RandomWords extends StatefulWidget {
-//   @override
-//   State<StatefulWidget> createState() {
-//     // TODO: implement createState
-//     new RandomWordsState();
-//   }
-// }
 class RandomWords extends StatefulWidget {
   @override
   createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
 }
 
 // 1.hello world
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
           title:  Text('bar title'),
         ),
         body:  Center(
-          child: Text('Hello World'),
+          child: RandomWords(),
         ),
       ),
     );
