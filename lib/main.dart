d0515cbf11576d56c834d75a3ba3a2650e99672d
GorCat <745318341@qq.com>
2019-07-22 15:11:54 +0800

    list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart' as prefix0;
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -8,17 +9,46 @@ void main() {
  runApp(MyApp());
 }
 
-// 2.package
+// 2.package 3.list view
 class RandomWords extends StatefulWidget {
   @override
   createState() => new RandomWordsState();
+  
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+        final index = i~/2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(prefix0.generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar:  AppBar(title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
 
@@ -28,14 +58,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'app title',
-      home:  Scaffold(
-        appBar: AppBar(
-          title:  Text('bar title'),
-        ),
-        body:  Center(
-          child: RandomWords(),
-        ),
-      ),
+      home:  RandomWords(),
     );
   }
 }
